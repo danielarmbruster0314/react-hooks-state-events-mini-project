5030a611c71046b64dd056f5de5351e88e11d0c3
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-18 10:56:23 -0600

    commit tree c629f2313cc29c160ba1a79cd6d3853b48fc8a18

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,138 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [category, setCategory] = useState("All");
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleDeleteTask(deletedTaskText) {
+    setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+  }
+
+  const visibleTasks = tasks.filter(
+    (task) => category === "All" || task.category === category
+  );
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={category}
+        onSelectCategory={setCategory}
+      />
+      <div className="tasks">
+        <h5>Tasks</h5>
+        <NewTaskForm
+          categories={CATEGORIES.filter((cat) => cat !== "All")}
+          onTaskFormSubmit={handleAddTask}
+        />
+        <TaskList onDeleteTask={handleDeleteTask} tasks={visibleTasks} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import CategoryFilter from "./CategoryFilter";
+// import NewTaskForm from "./NewTaskForm";
+// import TaskList from "./TaskList";
+
+// import { CATEGORIES, TASKS } from "../data";
+
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
+
+
+
+
+
+
+
+
+// function App() {
+//   const [tasks, setTasks] = useState(TASKS)
+//   const [category, setCategory] = useState("All")
+
+// const displayedTasks = tasks.filter(
+//   (task) => category === "All" || task.category === category
+// )
+
+
+
+// function removeFromList(toBeReomved){
+//   setTasks(tasks.filter( (task) => task.text !== toBeReomved))
+// }
+
+
+
+// function handleNewTask(newTask){
+//   setTasks([...tasks,newTask])
+// }
+
+
+
+//   return (
+//     <div className="App">
+//       <h2>My tasks</h2>
+//       <CategoryFilter disSelect={setCategory} selectedCat={category} category={CATEGORIES} />
+//       <NewTaskForm categor={CATEGORIES.filter((cat) => cat !== "All")} handleNewTask={handleNewTask}/>
+//       <TaskList tasks={displayedTasks} removeFromList={removeFromList}/>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,70 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onSelectCategory }) {
+  const categoryButtons = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : null;
+    return (
+      <button
+        key={category}
+        className={className}
+        onClick={() => onSelectCategory(category)}
+      >
+        {category}
+      </button>
+    );
+  });
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
 
 export default CategoryFilter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// function CategoryFilter({category, selectedCat, disSelect}) {
+
+
+// const catButton = category.map((cat) => {
+//   const classN = cat === selectedCat ? "seleced" : null ;
+// return (<button
+// key={cat}
+// className ={classN}
+// onClick={() => disSelect(cat)}
+// >{cat}</button>
+// )
+// })
+
+
+//   return (
+//     <div className="categories">
+//       <h5>Category filters</h5>
+//       {catButton}
+//     </div>
+//   );
+// }
+
+// export default CategoryFilter;
